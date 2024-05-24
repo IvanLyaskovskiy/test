ac44898a211d728a0e1f1bb1ea1b6583cd10d332
IvanLyaskovskiy <168165614+IvanLyaskovskiy@users.noreply.github.com>
2024-05-24 11:39:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
 package com.lessons;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println('dd');
+        Scanner scan = new Scanner(System.in);
+        String rightAnswer;
+        String userAnswer;
+        String[] questions = {"пити","жити","їсти","стрибати","вбивати","робити","писати","читати","плакати","посміхатися"};
+        String[] rightAnswers = {"beben", "vivir","comer","saltar","matar","hacer","escribir","leer","llorar","sonrisa"};
 
+        for(int i=0;i<10;i++){
+
+            System.out.print(questions[i] + " - ");
+            userAnswer = scan.nextLine();
+            rightAnswer = rightAnswers[i];
+
+            if(userAnswer.equals(rightAnswer)) {
+                System.out.println("Правильно!");
+            }
+            else{
+                System.out.println("Неправильно. Правильна відповідь: " + rightAnswer);
+            }
+        }
     }
 }
